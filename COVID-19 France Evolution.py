a2374e31e0fa17ad9014721aa4a8057a829e61d3
Marc Augier <m.augier@me.com>
2020-04-20 01:59:38 +0200

    France Evolution script and picture

---
+++ COVID-19 France Evolution.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+    Display COVID-19 Evolution in France
+    ====================================
+
+    (c) Marc Augier - XDM Consulting
+    m.augier@me.com
+
+"""
+
+df = pd.read_csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv").dropna()
+
+# Get French data
+df_fr = df[df.countriesAndTerritories == 'France']
+
+# Retain only date, cases and deaths columns
+df_fr = df_fr[['dateRep', 'cases', 'deaths']]
+
+# Calculate cumulative cases & deaths
+df_fr = df_fr.sort_values('dateRep')
+df_fr['cumCases'] = df_fr.cases.cumsum()
+df_fr['cumDeaths'] = df_fr.deaths.cumsum()
+
+import numpy as np
+
+x = np.arange(len(df_fr.dateRep))  # the label are dates
+width = 0.5  # the width of the bars
+
+fig, ax = plt.subplots(figsize=(10,12))
+rects1 = ax.bar(x - width/3, df_fr.cumCases, width, label='Cases')
+rects2 = ax.bar(x + width/3, df_fr.cumDeaths, width, label='Deaths')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Count')
+ax.set_title('Confirmed Cases and Deaths')
+ax.set_xticks(x)
+ax.set_xticklabels(df_fr.dateRep)
+ax.legend()
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+autolabel(rects1)
+autolabel(rects2)
+
+fig.tight_layout()
+
+plt.xticks(rotation=45)
+
+plt.show()
